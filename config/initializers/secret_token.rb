4eadb315c6b6b40d031f4a3a841af9e42ba2ac0a
hhuai <8863824@gmail.com>
2014-02-19 13:40:15 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qwrite::Application.config.secret_key_base = 'dc2adb7200757a0ea935debebe462af11cc7ec52a3801a2c55c5d4d6426e811969c04835a2e2f7b22f9e7bbf5e24b98f6d9f7b5fd76b280a6587f33b22d45a57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qwrite::Application.config.session_store :cookie_store, key: '_qwrite_session'
